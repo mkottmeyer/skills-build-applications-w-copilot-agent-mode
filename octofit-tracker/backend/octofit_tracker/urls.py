e54aecedbeeb8b050e97389d34d66366da4efff8
mkottmeyer <41296941+mkottmeyer@users.noreply.github.com>
2025-12-10 21:00:03 +0000

    Setup REST framework

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -30,12 +30,17 @@ router.register(r'leaderboard', views.LeaderboardViewSet)
 
 @api_view(['GET'])
 def api_root(request, format=None):
+    import os
+    codespace = os.environ.get('CODESPACE_NAME')
+    base_url = request.build_absolute_uri('/')
+    if codespace:
+        base_url = f'https://{codespace}-8000.app.github.dev/'
     return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'teams': reverse('team-list', request=request, format=format),
-        'activities': reverse('activity-list', request=request, format=format),
-        'workouts': reverse('workout-list', request=request, format=format),
-        'leaderboard': reverse('leaderboard-list', request=request, format=format),
+        'users': f'{base_url}api/users/',
+        'teams': f'{base_url}api/teams/',
+        'activities': f'{base_url}api/activities/',
+        'workouts': f'{base_url}api/workouts/',
+        'leaderboard': f'{base_url}api/leaderboard/',
     })
 
 urlpatterns = [
